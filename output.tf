755abfde22892aaa971e916f2deea6036894a3f6
Ubuntu <ubuntu@ip-172-31-34-91.ap-south-1.compute.internal>
2024-01-09 05:54:02 +0000

    terrform files

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "instance_state" {
+  description = "The state of the ec2 instance "
+  value       = module.ec2.instance_state
+}
+
+output "instance_public_dns" {
+  description = "The Public DNS address of the ec2 instance"
+  value       = module.ec2.instance_public_dns
+}
+
+output "instance_public_ip" {
+  description = "The Public Ip address of the ec2 instance"
+  value       = module.ec2.instance_public_ip
+}
+
+output "instance_eip" {
+  description = "EIP attach to the ec2 instance"
+  value       = module.ec2.instance_eip
+}
